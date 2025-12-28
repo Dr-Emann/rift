ec33ad40eb67ebae0f41214a84ebe78e73a404a9
Mohsen Zainalpour <zainalpour@gmail.com>
2025-12-28 15:20:25 -0500

    refactor: remove unnecessary #[allow(dead_code)] attributes (#27)

--- crates/rift-http-proxy/src/imposter/manager.rs
+++ crates/rift-http-proxy/src/imposter/manager.rs
@@ -21,7 +21,6 @@ pub struct ImposterManager {
     /// Active imposters by port
     imposters: RwLock<HashMap<u16, Arc<Imposter>>>,
     /// Global shutdown signal (for future graceful shutdown)
-    #[allow(dead_code)]
     shutdown_tx: broadcast::Sender<()>,
 }
 
@@ -213,7 +212,6 @@ impl ImposterManager {
     }
 
     /// Get imposter count (for future metrics)
-    #[allow(dead_code)]
     pub fn count(&self) -> usize {
         self.imposters.read().len()
     }
@@ -255,7 +253,6 @@ impl ImposterManager {
     }
 
     /// Shutdown all imposters (for future graceful shutdown)
-    #[allow(dead_code)]
     pub async fn shutdown(&self) {
         let _ = self.shutdown_tx.send(());
         self.delete_all().await;
